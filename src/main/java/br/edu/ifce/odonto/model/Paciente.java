fd218063fd3287c6dddc42af2398409056edf5e1
ramiresmoreira <ramires.nas@icloud.com>
2017-06-04 18:24:19 -0300

    adicionado busca de agendamentos por paciente

--- src/main/java/br/edu/ifce/odonto/model/Paciente.java
+++ src/main/java/br/edu/ifce/odonto/model/Paciente.java
@@ -1,9 +1,12 @@
 package br.edu.ifce.odonto.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Paciente {
@@ -13,6 +16,10 @@ public class Paciente {
 	private int id;
 	private String nome;
 	private int idade;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=javax.persistence.CascadeType.ALL)
+	@JoinColumn(name="dentista_id")
+	Dentista dentista;
 	
 	public Paciente() {
 	}
